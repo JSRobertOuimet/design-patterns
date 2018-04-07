262c7e3305e916130d9cc7a569ac2fd1602e06fa
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-04-06 23:21:36 -0400

    Updated decorator.js file.

---
+++ js/decorator.js
@@ -0,0 +1,15 @@
+// ==================================================
+// A decorator provides new methods to an object
+// ==================================================
+const o1 = {'key': 'value'};
+const o2 = {'key': 'value'};
+
+decorator(o2);
+
+console.log(o1);
+console.log('------------------------------');
+console.log(o2);
+
+function decorator(o) {
+  o.addedMethod = () => {};
+}

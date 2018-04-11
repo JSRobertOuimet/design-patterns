6707926a7b58990d6b4530d79a8bae720e27af65
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-04-11 15:57:22 -0400

    Updated flyweight.js file.

--- js/flyweight.js
+++ js/flyweight.js
@@ -1,10 +1,13 @@
+// The flyweight design pattern provides new properties
+// to an object without adding to the constructor
+
 const c1 = new Citizen('John Doe', 'male', 75);
 const c2 = new Citizen('Ann Smith', 'female', 34);
 const c3 = new Citizen('Robert Johnson', 'male', 45);
 
-flyweightComponent(c1, 50000);
-flyweightComponent(c2, 65000);
-flyweightComponent(c3, 40000);
+flyweight(c1, 50000);
+flyweight(c2, 65000);
+flyweight(c3, 40000);
 
 console.log(c1, c2, c3); // Only c1 has a property 'pensionVal' with corresponding value.
 
@@ -14,7 +17,7 @@ function Citizen(name, sex, age) {
   this.age = age;
 }
 
-function flyweightComponent(c, pensionVal) {
+function flyweight(c, pensionVal) {
   if(c.age >= 65) {
     c.pensionVal = pensionVal;
   }

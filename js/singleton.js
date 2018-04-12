4016a7e44dfc68635d66ea551c532ff8772a468c
Jean-Simon Robert Ouimet <jsrobertouimet@gmail.com>
2018-04-11 22:37:30 -0400

    Update singleton.js file.

--- js/singleton.js
+++ js/singleton.js
@@ -38,7 +38,7 @@
       if(!instance) {
         instance = {
           someMethod: () => {
-            console.log('I have been instantiated too.');
+            console.log('I have been instantiated.');
           }
         };
       }
@@ -47,14 +47,34 @@
     };
 
   })();
+
+  // Not checking if instance already exists
+  const NotASingleton = (() => {
+    let instance;
+
+    return () => {
+      instance = {
+        someMethod: () => {
+          return;
+        }
+      };
+
+      return instance;
+    };
+
+  })();
   
   const s1 = SingletonOne.getInstance();
   const s2 = SingletonOne.getInstance();
-
+  
   const s3 = SingletonTwo();
   const s4 = SingletonTwo();
-
-  console.log('Are s1 and s2 two instances of the same object?', Object.is(s1, s2));
-  console.log('Are s3 and s4 two instances of the same object?', Object.is(s3, s4));
+  
+  const s5 = NotASingleton();
+  const s6 = NotASingleton();
+  
+  console.log('Are s1 and s2 two instances of the same object?', s1 === s2);
+  console.log('Are s3 and s4 two instances of the same object?', s3 === s4);
+  console.log('Are s5 and s6 two instances of the same object?', s5 === s6);
 
 })(this);
